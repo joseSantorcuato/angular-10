4fd9a478716afea62b9c59239c6eceb4fd72c542
josesantorcuato <santorcuato76@gmail.com>
2020-08-28 16:17:01 -0400

    Parámetros. Paso de parámetros entre vistas para detalles. Breadcrums.

---
+++ 6parametros/src/app/componentes/catalogo/catalogo.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CatalogoService } from '../../servicios/catalogo.service';
+
+
+
+@Component({
+  selector: 'app-catalogo',
+  templateUrl: './catalogo.component.html',
+  styleUrls: ['./catalogo.component.css']
+})
+export class CatalogoComponent implements OnInit {
+
+  
+
+  searchText:string;
+  modelos:any[] = [];
+
+  constructor( private _catalogoService:CatalogoService) {
+
+  }
+
+  ngOnInit() {
+
+    this.modelos = this._catalogoService.getModelos();
+    console.log(this.modelos);
+
+  }
+
+
+
+}
---
+++ 6parametros/src/app/servicios/catalogo.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatalogoService {
+
+  private modelos:any[] = [
+
+{
+id: "1",
+nombre: "Escarabajo",
+ano: "1945",
+imagen: "assets/imagenes/escarabajo.jpg"
+},
+
+{
+id: "2",
+nombre: "Combi",
+ano: "1945",
+imagen: "assets/imagenes/combi.jpg"
+},
+
+{
+id: "3",
+nombre: "Brasilia",
+ano: "1945",
+imagen: "assets/imagenes/safari.jpg"
+}
+
+
+
+];
+
+
+  constructor() {
+ console.log("Servicio modelos funcionando perfectamente");
+
+  }
+
+  getModelos(){
+    return this.modelos;
+  }
+}
