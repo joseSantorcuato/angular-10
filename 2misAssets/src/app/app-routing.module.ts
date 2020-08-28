9cdd8a4a2de301076c8336cc3c9478ba817dc8ab
josesantorcuato <santorcuato76@gmail.com>
2020-08-27 20:52:40 -0400

    Integración de imágenes, media en general. Carpeta /assets, imágenes, favicon, brand.

---
+++ 2misAssets/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { Error404Component } from './componentes/error404/error404.component';
+import { CatalogoComponent } from './componentes/catalogo/catalogo.component';
+import { HistoriaComponent } from './componentes/historia/historia.component';
+
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/inicio', pathMatch: 'full' }, // cada vez que abre va a inicio
+  { path: 'inicio', component: InicioComponent },
+  { path: 'historia', component: HistoriaComponent },
+  { path: 'catalogo', component: CatalogoComponent },
+  { path: '**', component: Error404Component },  // en caso de no encontrar la pagina
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
