ffcfc7cbf0e629e8b1e940bc4ecf12f3b2421f6a
josesantorcuato <santorcuato76@gmail.com>
2020-08-30 22:22:29 -0400

    7 Parámetros 2. Utilización de decorador @input. Data binding entre componente y HTML. Función find() para filtrar resultados de array.

---
+++ 7passParams2/src/app/servicios/detalle.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DetallesService {
+  static seleccion(): any[] {
+    throw new Error("Method not implemented.");
+  }
+
+  private modelos:any[] = [
+
+{
+id: "1",
+nombre: "Escarabajo",
+ano: "1945",
+imagen: "assets/imagenes/escarabajo.jpg"
+},
+
+{
+id: "2",
+nombre: "Combi",
+ano: "1945",
+imagen: "assets/imagenes/combi.jpg"
+},
+
+{
+id: "3",
+nombre: "Brasilia",
+ano: "1945",
+imagen: "assets/imagenes/safari.jpg"
+}
+
+
+
+];
+
+
+  constructor() {
+ console.log("Servicio modelos funcionando perfectamente");
+
+  }
+
+  getModelos(){
+    return this.modelos;
+  }
+
+
+}
