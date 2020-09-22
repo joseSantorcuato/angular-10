564d3183fcade4790e4bf7e59bfc13ab32a99d2c
josesantorcuato <santorcuato76@gmail.com>
2020-09-22 14:25:01 -0300

    Se inicia proyecto de carro de compras, Angular 8.1.3

--- 9uddApp/src/environments/environment.ts
+++ 9uddApp/src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAVua9aBmKrfZ0iOXd08exhlyvQ3pkaRc4",
-    authDomain: "volkswagen-57a93.firebaseapp.com",
-    databaseURL: "https://volkswagen-57a93.firebaseio.com",
-    projectId: "volkswagen-57a93",
-    storageBucket: "volkswagen-57a93.appspot.com",
-    messagingSenderId: "1016324964305",
-    appId: "1:1016324964305:web:88ef3d63b487e844776af0"
+    apiKey: "xxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxx"
 
   }
 };
