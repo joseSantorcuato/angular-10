4ca0071032453f13c1a060d22a4ab7e42a8b908f
josesantorcuato <santorcuato76@gmail.com>
2020-09-29 16:54:15 -0300

    Se actualiza app udd

--- 9uddApp/src/app/login/login.component.ts
+++ 9uddApp/src/app/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../auth.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
-  ngOnInit() {
+  login() {
+
+    this.authService.login();
+    this.router.navigate(['products']);
+
   }
 
 }
--- 9uddApp/src/app/navbar/navbar.component.ts
+++ 9uddApp/src/app/navbar/navbar.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCartService } from './../shopping-cart.service';
+import { AppUser } from './../models/app-user';
+import { AuthService } from './../auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +12,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  appUser: AppUser;
+  cart$: Observable<ShoppingCart>;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private shoppingCartService: ShoppingCartService) {
+   }
 
-  ngOnInit() {
+  logout() {
+    this.auth.logout();
   }
-
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    this.cart$ = await this.shoppingCartService.getCart();
+  }
 }
